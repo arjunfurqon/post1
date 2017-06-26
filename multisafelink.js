4b264a0fdeeca816f71658c6dd3adc66a87e6d91
arjunfurqon <sent.miuipedia@gmail.com>
2017-06-27 02:13:13 +0700

    Update multisafelink.js

--- multisafelink.js
+++ multisafelink.js
@@ -21,7 +21,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="5-tips-to-buy-new-car-at-cheap-price"}
+if(x=="1"){xxx="5-tips-to-buy-new-car-at-cheap-price.html"}
 if(x=="2"){xxx="6-ways-to-start-property-business-from.html"}
 if(x=="3"){xxx="10-tips-how-to-live-healthy-lifestyle.html"}
 if(x=="4"){xxx="this-is-difference-between-sharia-and.html"}a_to_ve[i].href="https://safelinkv3-miuipedia.blogspot.com/2017/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
