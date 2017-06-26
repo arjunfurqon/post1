2a05f6da7089fcccaa89f5059bcf4d590d99a2dc
arjunfurqon <sent.miuipedia@gmail.com>
2017-06-26 12:52:36 +0700

    Update safelink-3-multiOK.js

--- safelink-3-multiOK.js
+++ safelink-3-multiOK.js
@@ -19,13 +19,12 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="5-tips-to-buy-new-car-at-cheap-price"}
 if(x=="2"){xxx="6-ways-to-start-property-business-from.html"}
 if(x=="3"){xxx="here-are-10-tips-how-to-live-healthy.html"}
-if(x=="4"){xxx="this-is-difference-between-sharia-and.html"}
-if(x=="5"){xxx="9-how-to-prevent-cancer-early-on.html"}a_to_ve[i].href="https://safelinkv3-miuipedia.blogspot.com/2017/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="this-is-difference-between-sharia-and.html"}a_to_ve[i].href="https://safelinkv3-miuipedia.blogspot.com/2017/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
