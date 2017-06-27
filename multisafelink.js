ca579b032ce568b4374ca99b590439708e28d6ff
arjunfurqon <sent.miuipedia@gmail.com>
2017-06-27 13:03:14 +0700

    Update multisafelink.js

--- multisafelink.js
+++ multisafelink.js
@@ -24,8 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="5-tips-to-buy-new-car-at-cheap-price.html"}
 if(x=="2"){xxx="6-ways-to-start-property-business-from.html"}
 if(x=="3"){xxx="this-is-difference-between-sharia-and.html"}
-if(x=="4"){xxx="10-tips-how-to-live-healthy-lifestyle.html"}
-a_to_ve[i].href="https://miuisafelink.blogspot.com/2017/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="10-tips-how-to-live-healthy-lifestyle.html"}a_to_ve[i].href="https://miuisafelink.blogspot.com/2017/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
